ccfdc8795373166152666cb5b1292376ba8d776e
Chris Bull <chris@chrisbull.com>
2021-09-22 11:51:34 -0700

    update test

--- App.test.tsx
+++ App.test.tsx
@@ -1,8 +1,15 @@
+import renderer from 'react-test-renderer'
 import React from 'react'
-import { create } from 'react-test-renderer'
 import App from './App'
 
-test('renders correctly', () => {
-  const tree = create(<App />).toJSON()
-  expect(tree).toMatchSnapshot()
+describe('<App />', () => {
+  it('has 1 child', () => {
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree.children.length).toBe(1)
+  })
+
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
